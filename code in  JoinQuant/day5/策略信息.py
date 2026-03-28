42f8f5414e89d16e59a476c9b742339d51337f15
jmxiao1 <1768472720@qq.com>
2026-03-28 15:10:58 +0800

    策略信息
    发现虽然无法调用聚宽的库，但毕竟符合python格式只保存不会保存，所以之后也上传python文件了。
    
    今日学习策略信息
    context对象：策略信息总览，包括账户，时间等信息。比如subportfolios(当前单个操作仓位的资金，标的信息，是一个subportfolio的数组），portfolio(账户信息，即subportfolio的汇总信息，Portfolio对象，单个操作仓位时，portfolio指向subportfolio[0]) ，current_dt(当前单位时间的开始时，[datetime.datetime]对象） ，previous_date(前一个交易日，[datetime.date]对象，注意，这是一个日期，是date，而不是datetime） , universe(查询set——universe()设定的股票池，比如['000001.XSHE','600000.XSHE'])
    
    Position对象：输出持有标的的信息，包括security(标的信息）,price(最新行情价格）,total_amount(总仓位，不包括挂单冻结仓位） ，init_time(建仓时间，格式为datetime.datetime)

---
+++ code in  JoinQuant/day5/策略信息.py
@@ -0,0 +1,28 @@
+# 导入函数库
+from jqdata import *
+
+# 初始化函数，设定基准等等
+def initialize(context):
+    # 设定沪深300作为基准
+    set_benchmark('000300.XSHG')
+    g.security ='000001.XSHE'
+    ##run_weekly(market_open,1,time='open')
+    run_daily(after_market_close,time='15:30')
+
+def handle_data(context, data):
+    ##context.portfolio变为整数1
+    #context.portfolio = 1
+    #lgo.info(context.portfolio.total_value)
+
+    
+    #总资产
+    log.info(context.portfolio.total_value)
+    #持仓余额
+    log.info(context.portfolio.positions_value)
+    #今日日期
+    log.info(context.current_dt.day)
+    #总权益的累计收益
+    log.info(context.portfolio.returns)
+    #获取仓位subportfolios[0]的可用资金
+    
+    
